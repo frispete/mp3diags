60f88e2a4b7c881c37de44b87913dfeeda27d6b9
ciobi07 <ciobi07@f8aa0ff1-aaa6-4a14-9261-7bf0db1e1f66>
2014-10-03 08:27:43 +0000

    better error reporting and elimination of some asserts

--- mp3diags/src/Helpers.cpp
+++ mp3diags/src/Helpers.cpp
@@ -24,6 +24,7 @@
 #include  "fstream_unicode.h"
 #include  <sstream>
 #include  <iomanip>
+#include  <cstdio>
 
 #include  <boost/version.hpp>
 
@@ -64,6 +65,17 @@ void assertBreakpoint()
     //qDebug("assert");
 }
 
+CbException::CbException(std::string strMsg, const char* szFile, int nLine) {
+    vector<char> a (strMsg.size() + strlen(szFile) + 20);
+    sprintf(&a[0], "%s [%s/%d]", strMsg.c_str(), szFile, nLine);
+    m_strMsg = &a[0];
+}
+
+CbException::CbException(std::string strMsg, const char* szFile, int nLine, const CbException& cause) {
+    vector<char> a (strMsg.size() + strlen(szFile) + cause.m_strMsg.length() + 40);
+    sprintf(&a[0], "%s [%s/%d] / Caused by: %s", cause.m_strMsg.c_str(), szFile, nLine, cause.what());
+    m_strMsg = &a[0];
+}
 
 
 void appendFilePart(istream& in, ostream& out, streampos pos, streamoff nSize)
